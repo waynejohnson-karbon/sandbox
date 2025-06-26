ff9755c15ebafd9d5f826ada76f949d37fb778e2
Wayne Johnson <wayne.johnson@karbonhq.com>
2025-06-26 15:01:28 +1000

    Works locally

--- public/lokalise-test-script.js
+++ public/lokalise-test-script.js
@@ -1,6 +1,13 @@
-import lokaliseDevhub from '@api/lokalise-devhub';
+import { LokaliseApi } from "@lokalise/node-api";
 
-lokaliseDevhub.auth('b56292353456ae5b0eb793dabb1f4cb7f73fa85b');
-lokaliseDevhub.listAllProcesses({project_id: '66835468677f4447c39f72.08805740'})
-  .then(({ data }) => console.log('script console output: ', data))
-  .catch(err => console.error('console error: ', err));
+const lokaliseApi = new LokaliseApi({
+  apiKey: 'b56292353456ae5b0eb793dabb1f4cb7f73fa85b'
+});
+
+const projects = await lokaliseApi.projects().list();
+console.log('script console output: ', projects);
+
+// lokaliseDevhub.auth('b56292353456ae5b0eb793dabb1f4cb7f73fa85b');
+// lokaliseDevhub.listAllProcesses({project_id: '66835468677f4447c39f72.08805740'})
+//   .then(({ data }) => console.log('script console output: ', data))
+//   .catch(err => console.error('console error: ', err));
