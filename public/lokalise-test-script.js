b0ae17cba4f469cf8abf108512a346a2828d7686
Wayne Johnson <wayne.johnson@karbonhq.com>
2025-06-26 15:39:59 +1000

    Processes

--- public/lokalise-test-script.js
+++ public/lokalise-test-script.js
@@ -4,8 +4,11 @@ const lokaliseApi = new LokaliseApi({
   apiKey: 'b56292353456ae5b0eb793dabb1f4cb7f73fa85b'
 });
 
-const projects = await lokaliseApi.projects().list();
-console.log('script console output: ', projects);
+const processes = await lokaliseApi.queuedProcesses().list({
+  project_id: '66835468677f4447c39f72.08805740',
+});
+
+console.log('script console output: ', processes);
 
 // lokaliseDevhub.auth('b56292353456ae5b0eb793dabb1f4cb7f73fa85b');
 // lokaliseDevhub.listAllProcesses({project_id: '66835468677f4447c39f72.08805740'})
