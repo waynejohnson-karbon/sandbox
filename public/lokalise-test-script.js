9801e03188a8e556295c80980f52356b977c8869
Wayne Johnson <wayne.johnson@karbonhq.com>
2025-06-26 16:01:53 +1000

    Work hours and flags to know when to run.

--- public/lokalise-test-script.js
+++ public/lokalise-test-script.js
@@ -8,7 +8,10 @@ const processes = await lokaliseApi.queuedProcesses().list({
   project_id: '66835468677f4447c39f72.08805740',
 });
 
-console.log('script console output: ', processes);
+const readyToCreatePR = processes.items[processes.items.length-1].type === 'file-import'
+
+console.log('ready To Create PR: ', readyToCreatePR);
+
 
 // lokaliseDevhub.auth('b56292353456ae5b0eb793dabb1f4cb7f73fa85b');
 // lokaliseDevhub.listAllProcesses({project_id: '66835468677f4447c39f72.08805740'})
