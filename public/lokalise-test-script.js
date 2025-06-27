736154fac03b63cbdb9e14b702eaa0baa43c52c4
Wayne Johnson <wayne.johnson@karbonhq.com>
2025-06-27 10:04:43 +1000

    Create a PR.

--- public/lokalise-test-script.js
+++ public/lokalise-test-script.js
@@ -11,9 +11,32 @@ const processes = await lokaliseApi.queuedProcesses().list({
 const readyToCreatePR = processes.items[processes.items.length-1].type === 'file-import'
 
 console.log('ready To Create PR: ', readyToCreatePR);
+if (readyToCreatePR){
+  const process = await lokaliseApi.files().async_download('66835468677f4447c39f72.08805740',
+    {
+      format: 'yaml', 
+      "original_filenames": true,
+      "directory_prefix": "/",
+      "all_platforms": true,
+      "export_sort": "a_z",
+      "export_empty_as": "empty",
+      "include_comments": false,
+      "include_description": false,
+      "replace_breaks": false,
+      "disable_references": false,
+      "plural_format": "icu",
+      "placeholder_format": "icu",
+      "icu_numeric": true,
+      "indentation": "2sp",
+      "yaml_include_root": false,
+      "triggers": [
+        "github"
+      ]
+    }
+  );
 
+const process_id = process.process_id;
 
-// lokaliseDevhub.auth('b56292353456ae5b0eb793dabb1f4cb7f73fa85b');
-// lokaliseDevhub.listAllProcesses({project_id: '66835468677f4447c39f72.08805740'})
-//   .then(({ data }) => console.log('script console output: ', data))
-//   .catch(err => console.error('console error: ', err));
+} else {
+  console.log('Already downloaded. No new content available to make a PR.');
+}
